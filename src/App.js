8cc2c838ed4f040716b8a9df6ade562bf4bbac20
dian1p <p_naidz@yahoo.com>
2020-03-01 21:03:22 +0100

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+        <h1>Weather App</h1>
+        <a href="https://github.com/dian1p/react-weather-app"
+        target="_blank" rel="noopener noreferrer">{" "}Open Source GitHub</a> by Dian 
+      </div>
     </div>
   );
 }
